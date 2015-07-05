994fae1d89fa6050be117bcbb413ab4fc2c83a98
Hossam-Hazem <hossamhazem94@gmail.com>
2015-07-05 21:20:08 +0200

    Header Complete

--- public/js/home.js
+++ public/js/home.js
@@ -1,74 +1,89 @@
-$(document).ready(function(){
-    console.log('document width = '+$(document).width()+' height = '+$(window).height())
-
+$(document).ready(function () {
+    //some modernizer thing
+    var animEndEventNames = {
+            'WebkitAnimation' : 'webkitAnimationEnd',
+            'OAnimation' : 'oAnimationEnd',
+            'msAnimation' : 'MSAnimationEnd',
+            'animation' : 'animationend'
+        },
+        animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ]
+    ///////////
     var width = $(document).width();
     $('.WelcomeHead').width(width);
     $('.WelcomeHead').height($(window).height());
+    $('.infoDiv').height($(window).height() - $('.WelcomeHeadHeader').height() - 80);
     scrollingDiv();
-    var c=0;
-    var l=$('.scrolling').children().length;
-   var scrollinginterval=setInterval(function(){
-        scrollIt('down',c,l)
-        c=(c+1)%l
-    },3000)
+    var c = 0;
+    var l = $('.scrolling').children().length;
+    var scrollinginterval = setInterval(function () {
+        scrollIt('down', c, l)
+        c = (c + 1) % l
+    }, 3000)
+    ///////////////////////
+
+    $('.HomeBody').addClass('hidden');
 
 
-
-
-
-
-
-
-
-
-    $(window).resize(function() {
+    $(window).resize(function () {
         $('.WelcomeHead').width($(window).width());
         $('.WelcomeHead').height($(window).height());
+        $('.infoDiv').height($(window).height() - $('.WelcomeHeadHeader').height() - 80);
+        console.log($('.infoDiv').height())
     });
-    $('.glyphicon-circle-arrow-down').click(function(){
-        scrollIt('down',c,l);
-        c=(c+1)%l;
+    $('.glyphicon-circle-arrow-down').click(function () {
+        scrollIt('down', c, l);
+        c = (c + 1) % l;
         clearInterval(scrollinginterval);
-        scrollinginterval= setInterval(function(){
-            scrollIt('down',c,l)
-            c=(c+1)%l
-        },3000)
+        scrollinginterval = setInterval(function () {
+            scrollIt('down', c, l)
+            c = (c + 1) % l
+        }, 3000)
     })
-    $('.glyphicon-circle-arrow-up').click(function(){
-        scrollIt('up',c,l);
-        c=c-1;
-        if(c==-1)
-            c=l-1
+    $('.glyphicon-circle-arrow-up').click(function () {
+        scrollIt('up', c, l);
+        c = c - 1;
+        if (c == -1)
+            c = l - 1
         clearInterval(scrollinginterval);
-        scrollinginterval= setInterval(function(){
-            scrollIt('down',c,l)
-            c=(c+1)%l
-        },3000)
+        scrollinginterval = setInterval(function () {
+            scrollIt('down', c, l)
+            c = (c + 1) % l
+        }, 3000)
+    })
+    $('.BodyArrowouterDiv').click(function () {
+        $('body').addClass('bodyAnimate');
+        $('.WelcomeHead').addClass('AnimateUpOut');
+        $('.HomeBody').removeClass('hidden');
+        $('.HomeBody').addClass('AnimateUpIn');
+        $('body').on(animEndEventName,function(){
+            $('.WelcomeHead').removeClass('AnimateUpOut');
+            $('.HomeBody').removeClass('AnimateUpIn');
+            $('.WelcomeHead').addClass('hidden');
+            $('body').removeClass('bodyAnimate');
+        })
     })
 })
 
 
-function scrollingDiv(){
-    $('.scrolling').children().not('#e0,#e1,#e2,#e3,#e4').css('display','none').addClass('hidden');
+function scrollingDiv() {
+    $('.scrolling').children().not('#e0,#e1,#e2,#e3,#e4').css('display', 'none').addClass('hidden');
     $('.scrolling>#e0').addClass('LowOpacity');
     $('.scrolling>#e1').addClass('MedOpacity');
     $('.scrolling>#e3').addClass('MedOpacity');
     $('.scrolling>#e4').addClass('LowOpacity');
-    $('.scrolling').css('min-height',$('.scrolling').css('height'))
+    $('.scrolling').css('min-height', $('.scrolling').css('height'))
 }
-function scrollIt(type,c,l){
-    if(type=='down') {
+function scrollIt(type, c, l) {
+    if (type == 'down') {
         var $tmpout = $('.scrolling>#e' + c);
         cn = (c + 1) % l;
         var $tmpin = $('.scrolling>#e' + ((cn + 4) % l))
-        console.log($tmpin)
         $tmpout.slideUp('slow', function () {
             $tmpout.addClass('hidden')
-            console.log('c = ' + c + ' cn = ' + cn)
             $('.scrolling>#e' + c).removeClass('LowOpacity');
-            $('.scrolling>#e' + (c + 4)% l).removeClass('LowOpacity');
-            $('.scrolling>#e' + (c + 1)% l).removeClass('MedOpacity');
-            $('.scrolling>#e' + (c + 3)% l).removeClass('MedOpacity');
+            $('.scrolling>#e' + (c + 4) % l).removeClass('LowOpacity');
+            $('.scrolling>#e' + (c + 1) % l).removeClass('MedOpacity');
+            $('.scrolling>#e' + (c + 3) % l).removeClass('MedOpacity');
             $tmpout.remove();
             $('.scrolling').append($tmpout);
 
@@ -81,28 +96,26 @@ function scrollIt(type,c,l){
 
         });
     }
-    else{
-       cn=c-1;
-       if(cn==-1) {
-           cn = l - 1;
-       }
-        var $tmpin= $('.scrolling>#e' + cn);
-        var $tmpout=$('.scrolling>#e' + (c+4)%l);
-        console.log($tmpin)
-        console.log($tmpout)
-        $tmpout.fadeOut('slow',function(){
+    else {
+        cn = c - 1;
+        if (cn == -1) {
+            cn = l - 1;
+        }
+        var $tmpin = $('.scrolling>#e' + cn);
+        var $tmpout = $('.scrolling>#e' + (c + 4) % l);
+        $tmpout.fadeOut('slow', function () {
             $tmpout.addClass('hidden')
             $('.scrolling>#e' + c).removeClass('LowOpacity');
-            $('.scrolling>#e' + (c+1)%l).removeClass('MedOpacity');
-            $('.scrolling>#e' + (c+3)%l).removeClass('MedOpacity');
-            $('.scrolling>#e' + (c+4)%l).removeClass('LowOpacity');
+            $('.scrolling>#e' + (c + 1) % l).removeClass('MedOpacity');
+            $('.scrolling>#e' + (c + 3) % l).removeClass('MedOpacity');
+            $('.scrolling>#e' + (c + 4) % l).removeClass('LowOpacity');
 
             $tmpin.remove();
             $('.scrolling').prepend($tmpin);
             $('.scrolling>#e' + cn).addClass('LowOpacity');
             $('.scrolling>#e' + c).addClass('MedOpacity');
-            $('.scrolling>#e' + (c+2)%l).addClass('MedOpacity');
-            $('.scrolling>#e' + (c+3)%l).addClass('LowOpacity');
+            $('.scrolling>#e' + (c + 2) % l).addClass('MedOpacity');
+            $('.scrolling>#e' + (c + 3) % l).addClass('LowOpacity');
             $tmpin.removeClass('hidden');
             $tmpin.fadeIn('slow');
         })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,48 +5,78 @@
     <title>Hossam Hazem</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Poiret One">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Oswald">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/89f2f44ba07ea3fff7e561c2142813b278c2d6c6/devicon.min.css">
     <link rel="stylesheet" href="{{asset('css/home.css')}}"/>
 </head>
 <body>
-<div class = "WelcomeDiv"></div>
+<div class="WelcomeDiv"></div>
 
-<div class = "WelcomeHead">
-        <div class="WelcomeTag">Welcome</div>
-        <div class="WelcomeImagediv"><img src="{{asset('images/myimage.jpg')}}" class="WelcomeImage"/></div>
-        <div class="infoDiv">
-            <h1 class="glyphicon glyphicon-circle-arrow-up"></h1>
-            <div class="scrolling">
-                <div class="centertext WelcomeText"id="e0">0</div>
-                <div class="centertext WelcomeText"id="e1">1</div>
-                <div class="centertext WelcomeText"id="e2">2</div>
-                <div class="centertext WelcomeText"id="e3">3</div>
-                <div class="centertext WelcomeText"id="e4">4</div>
-                <div class="centertext WelcomeText"id="e5">5</div>
-                <div class="centertext WelcomeText"id="e6">6</div>
-                <div class="centertext WelcomeText"id="e7">7</div>
-            </div>
-            <h1 class="glyphicon glyphicon-circle-arrow-down"></h1>
+<div class="WelcomeHead">
+    <div class="WelcomeLayer">
+        <div class="WelcomeHeadHeader">
+            <div class="WelcomeTag">Welcome</div>
+            <div class="WelcomeImagediv"><img src="{{asset('images/myimage.jpg')}}" class="WelcomeImage"/></div>
         </div>
+        <div class="infoDiv">
+            <h4 class="glyphicon glyphicon-circle-arrow-up"></h4>
 
-        <div class="ArrowDownDiv"><img src="{{asset('images/Arrow-Down.png')}}" class="ArrowDown"/></div>
+            <div class="scrolling">
+                <div class="centertext WelcomeText" id="e0">German University in Cairo Student</div>
+                <div class="centertext WelcomeText" id="e1">Computer Science and Engineering Victim</div>
+                <div class="centertext WelcomeText" id="e2">Software Developer</div>
+                <div class="centertext WelcomeText" id="e3">Web Wizard</div>
+                <div class="centertext WelcomeText" id="e4">6th Semester Survivor</div>
+                <div class="centertext WelcomeText" id="e5">2017 Expected Graduate</div>
+                <div class="centertext WelcomeText" id="e6">Excellent Grade Student</div>
+                <div class="centertext WelcomeText" id="e7">Ex Intern at Raya</div>
+                <div class="centertext WelcomeText" id="e8">Hard Worker</div>
+                <div class="centertext WelcomeText" id="e9">Fast Learner</div>
+            </div>
+            <h4 class="glyphicon glyphicon-circle-arrow-down"></h4>
+        </div>
+        <footer class="BodyArrowouterDiv">
+            <div><h1 class="glyphicon glyphicon-menu-down BodyArrow"></h1></div>
+            <div><h1 class="BodyArrow BodyArrow2 glyphicon glyphicon-menu-down "></h1></div>
+        </footer>
+    </div>
+</div>
+<div class="HomeBody">
+    <header>
+        <nav class="HBNav">
+            <div class="HBNavX">
+                <img class='HBNavXImage' src="{{asset('images/myimage.jpg')}}"/></img>
+                <div class="HBNavXName">
+                    <span class="HBNavXName">Hossam Hazem</span>
+                </div>
+            </div>
+            <div class="HBNavMenu">
+                <div class="HBNavItem"><a class="HBNavItemLink" href="">Skills</a></div>
+                <div class="HBNavItem"><a class="HBNavItemLink" href="">My Projects</a></div>
+                <div class="HBNavItem" ><a class="HBNavItemLink" href="">School Projects</a></div>
+                <div class="HBNavItem" ><a class="HBNavItemLink" href="">Contact me</a></div>
+            </div>
+        </nav>
+    </header>
+    <div class="HomeContent">
+        <section class="Skills">
+            <header class="SkillsHeader">Skills & Experience</header>
+
+        </section>
+
+        <section class="MyProjects"></section>
+
+        <section class="SchoolProjects"></section>
+
+        <section class="ContactMe"></section>
+    </div>
+    <footer></footer>
+
 
 </div>
-
-<header>haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa3</header>
-
-<section class="About"></section>
-
-<section class="Skills"></section>
-
-<section class="MyProjects"></section>
-
-<section class="SchoolProjects"></section>
-
-<section class="ContactMe"></section>
-
-<footer></footer>
 <script src="{{ asset('js/jquery-1.11.3.js') }}"></script>
 <script src="{{ asset('js/home.js') }}"></script>
+<script src="{{ asset('js/modernizr.custom.js') }}"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
