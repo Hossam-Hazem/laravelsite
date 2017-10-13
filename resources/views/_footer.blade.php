3d74f6fb64f24af52f079e7353c6a3d4f86cbfb5
hossam-hazem <hossamhazem94@gmail.com>
2017-10-14 00:03:31 +0200

    add a note

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -10,5 +10,6 @@
         <div class="footerThing footerEmail myHidden">hossam.yehya@guc.edu.eg</div>
         <div class="footerThing footerMobile myHidden">01008761826</div>
         <div class="omdonya">صنع في مصر</div>
+        <div style="color: #E74C3C;">This portfolio was created in 2015, only data and minor things were updated since.</div>
     </div>
 </footer>
